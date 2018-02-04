50ad83f233d12951b22a3b70faee67f542455f59
John Wolfe <johnwolfe@Johns-MacBook-Pro-2.local>
2018-02-04 08:26:09 -0500

    added express

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+  })
+
+
+app.listen(3000, function() {
+    console.log('listening on 3000')
+  })

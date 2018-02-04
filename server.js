5c2ee5bf4a1795108d6707918c60ba3118f55031
John Wolfe <johnwolfe@Johns-MacBook-Pro-2.local>
2018-02-04 09:40:35 -0500

    connection error

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 const express = require('express');
+const bodyParser= require('body-parser')
 const app = express();
+const MongoClient = require('mongodb').MongoClient
 
+app.use(bodyParser.urlencoded({extended: true}))
+
+var db
+
+MongoClient.connect('mongodb://<jwolfe890>:<Hytrax890>@ds125068.mlab.com:25068/1stdb', (err, client) => {
+    console.log("hello")
+    if (err) return console.log(err)
+    db = client.db('1stdb') // whatever your database name is
+    app.listen(3000, () => {
+      console.log('listening on 3000')
+    })
+  })
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+app.post('/quotes', (req, res) => {
+    db.collection('quotes').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+    
+        console.log('saved to database')
+        res.redirect('/')
+      })
+  })
 
 app.listen(3000, function() {
     console.log('listening on 3000')
   })
+
+
